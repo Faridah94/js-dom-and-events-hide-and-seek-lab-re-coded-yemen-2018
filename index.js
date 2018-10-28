63ef7ed76ec05330ce6695fb7dd2b2b74cf0dfa5
Farida <faridah.absi@gmail.com>
2018-10-28 10:26:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ function getFirstSelector(selector){
   return document.querySelector(selector);
  }
 function nestedTarget(){
- 
+
   let target =document.querySelector("#nested").querySelector(".target");
    return target;
 }
 function increaseRankBy(n) {
     let childrens = document.querySelectorAll(".ranked-list li");
-    
+
      for (let i = 0; i < childrens.length; i++) {
         let result = parseInt(childrens[i].innerHTML);
         childrens.innerHTML = result + n ;
@@ -18,5 +18,5 @@ function increaseRankBy(n) {
 function deepestChild(){
   let child =document.querySelector("#grand-node div div div div");
   return child;
-  
+
 }
